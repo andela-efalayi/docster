9682f62819300fc41f556a613e020dc79adec783
Esther Falayi <estherfalayi@Esther-Falayi.local>
2017-06-24 16:54:52 +0100

    chore(147794767): login and logout routes test - fix users route test - create test cases for login and logout routes - fix eslint errors [Finishes #147794767]

--- server/__tests__/user/userRoutes.spec.js
+++ server/__tests__/user/userRoutes.spec.js
@@ -2,21 +2,74 @@ import chai from 'chai';
 import chaiHttp from 'chai-http';
 import colors from 'colors';
 import serverData from '../fakerData/server-data';
-import model from '../../models';
 import server from '../../server';
 
 const expect = chai.expect;
-const User = model.User;
+let newuser = serverData.newUser;
 let createdUser;
+let serverResponse;
 
 process.env.NODE_ENV = 'test';
 chai.use(chaiHttp);
 
 describe(colors.green('UserRoutes'), () => {
+  describe(colors.underline('POST /users'), () => {
+    it('should create a new user in database', (done) => {
+      chai.request(server)
+      .post('/users')
+      .send(newuser)
+      .end((err, res) => {
+        serverResponse = res.body;
+        createdUser = serverResponse.user;
+        expect(res.status).to.equal(201);
+        expect(serverResponse.message)
+        .to.equal('User was created successfully');
+        done();
+      });
+    });
+  });
+  describe(colors.underline('POST /users/login'), () => {
+    it('should check if user exists in the database', (done) => {
+      const user = newuser.email;
+      const password = newuser.password;
+
+      chai.request(server)
+      .post('/users/login')
+      .send({user, password})
+      .end((err, res) => {
+        expect(res.status).to.equal(201);
+        expect(res.body.message)
+        .to.equal('User was logged in successfully');
+        done();
+      });
+    });
+  });
+  describe(colors.underline('POST /users/logout'), () => {
+    it('should check if user exists in the database', (done) => {
+      chai.request(server)
+      .post('/users/logout')
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.message)
+        .to.equal('User is logged out');
+        done();
+      });
+    });
+  });
   describe(colors.underline('GET /users'), () => {
-    it('should get all users from database', (done) => {
+    it('should return an error if no token is provided', (done) => {
       chai.request(server)
       .get('/users')
+      .end((err, res) => {
+        expect(res.status).to.equal(403);
+        expect(res.body.message).to.equal('No token provided');
+        done();
+      });
+    });
+    it('should return users if token is provided', (done) => {
+      chai.request(server)
+      .get('/users')
+      .set('authorisation', 'Bearer '+serverResponse.token)
       .end((err, res) => {
         expect(res.status).to.equal(200);
         expect(res.body.message).to.equal('Users were retrieved successfully');
@@ -24,40 +77,13 @@ describe(colors.green('UserRoutes'), () => {
       });
     });
   });
-  describe(colors.underline('POST /users'), () => {
-    it('should create a new user in database', (done) => {
-      chai.request(server)
-      .post('/users')
-      .send(serverData.newUser)
-      .end((err, res) => {
-        expect(res.status).to.equal(201);
-        expect(res.body.message).to.equal('User was created successfully');
-        done();
-      });
-    });
-  });
   describe(colors.underline('GET /users/:userId'), () => {
-    it('should get a user with the id specified', (done) => {
-      User.create(serverData.appUser)
-      .then((user) => {
-        createdUser = user.dataValues;
-        chai.request(server)
-        .get(`/users/${createdUser.id}`)
-        .end((err, res) => {
-          expect(res.status).to.equal(200);
-          expect(res.body.fullName).to.equal(createdUser.fullName);
-          done();
-        });
-      });
-    });
-    it('should give an error if user does not exist', (done) => {
-      chai.request(server)
-      .get('/users/-1')
-      .end((err, res) => {
-        expect(res.status).to.equal(404);
-        expect(res.body.message).to.equal('User not found');
-        done();
-      });
+    it('should return an error if no token is provided', (done) => {
+    chai.request(server)
+    .get(`/users/${createdUser.id}`)
+    .end((err, res) => {
+      expect(res.status).to.equal(403);
+      done();
     });
   });
   describe(colors.underline('PUT /users/:userId'), () => {
@@ -104,4 +130,5 @@ describe(colors.green('UserRoutes'), () => {
       });
     });
   });
+});
 });
