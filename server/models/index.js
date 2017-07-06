697868c958e9c5d83f473dbe81dc54c7206fef56
Esther Falayi <estherfalayi@Esther-Falayi.local>
2017-07-06 23:50:29 +0100

    fix heroku error

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const configPath = require('../config/config');
+const configPath = require('../config/config.js');
 
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
