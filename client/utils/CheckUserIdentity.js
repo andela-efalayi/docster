413b196bddfbff201d035175b4f643e4079d0c7e
Esther Falayi <estherfalayi@Esther-Falayi.local>
2017-07-12 01:59:41 +0100

    fix /allusers route bug

--- client/utils/CheckUserIdentity.js
+++ client/utils/CheckUserIdentity.js
@@ -1,16 +1,17 @@
-const user = JSON.parse(localStorage.getItem('currentUser'));
-
 /**
  * Returns true if user roleId is 1
  * @returns {bool} isAdmin
  */
 export function isAdmin() {
+  const user = JSON.parse(localStorage.getItem('currentUser'));  
   return user.roleId === 1;
 }
 
 /**
+ * @param {number} userId
+ * @param {number} documentUserId
  * @returns {bool} isOwner
  */
-export function isOwner() {
-  return user.roleId;
+export function isOwner(userId, documentUserId) {
+  return userId === documentUserId;
 }
