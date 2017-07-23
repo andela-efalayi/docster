ec51f4febf7d40d69b23043d990a2e55c7c76635
Esther Falayi <estherfalayi@Esther-Falayi.local>
2017-07-23 23:05:53 +0100

    debug

--- server/server.js
+++ server/server.js
@@ -23,9 +23,9 @@ if(process.env.NODE_ENV === 'production') {
   console.log("production");
   
   app.use(express.static('build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-  });
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.join(__dirname, '../public/index.html'));
+  // });
  } //else {
 //   console.log("devlopement");
 //   devServer(app);
